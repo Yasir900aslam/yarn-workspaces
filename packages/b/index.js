5bbe213b165088386f98d7b0f5e863f5899c13a8
yasir <yasir900aslam@gmail.com>
2021-05-21 17:30:37 +0500

    Added C packages that is a dependency for a package.

--- packages/b/index.js
+++ packages/b/index.js
@@ -1,3 +1,4 @@
-const { double } = require('a')
+const { double, sum } = require('a')
 
-console.log(double(2));
+console.log(`Double of 2 is ${double(2)}`)
+console.log(`Sum of 2 and 4 is ${sum(2, 4)}`)
